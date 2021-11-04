85558ade0538b32399e3b6721a2b2b5046dbb17a
Park-min-hyoung <qkralswgud1324@naver.com>
2021-11-04 16:58:09 +0900

    plus

--- 배열/두 수의 합.py
+++ 배열/두 수의 합.py
@@ -1,8 +1,28 @@
 #1
-nums = [2,7,14,9]
+nums = [2,7,11,15]
 target = 9
 
-result = []
+# 첫 번째 수를 뺀 결과 키 조회(집합사용)(틀렸음)
+# key와 value를 뒤집음으로써 index를 value로 사용(어차피 출력결과는 index 번호이므로...)
+# 여기 책 개념부터 훑어 보기
+'''dict = {}
+for i, num in enumerate(nums):
+    dict[num] = i
+
+for i, num in enumerate(nums):
+    if target - num in dict and i != dict[target - num]:
+        print([i, dict[target - num]])
+        exit()'''
+
+#in 사용(틀렸음)
+'''for i, n in enumerate(nums):
+    comp = target - n
+
+    if comp in nums[i + 1:]:
+        print([nums.index(n), nums[i + 1:].index(comp) + (i + 1)])'''
+
+#브루트 포스로 계산
+'''result = []
 for i in range(len(nums)):
     for j in range(i + 1, len(nums)):
         if target == nums[i] + nums[j]:
@@ -10,4 +30,4 @@ for i in range(len(nums)):
 
 f_result = ' '.join(map(str, result))
 
-print(f_result)
+print(f_result)'''
---
+++ 연결 리스트/팰린드롬 연결 리스트.py
@@ -0,0 +1,79 @@
+from typing import List
+from collections import deque
+
+c = []
+a = c
+
+a.append(5)
+print(a, c)
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+#런너를 이용한 우아한 풀이
+'''class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        rev = None
+
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+
+            rev, rev.next, slow = slow, rev, slow.next
+
+        if fast:
+            slow = slow.next
+
+        while rev and rev.val == slow.val:
+            rev = rev.next
+            slow = slow.next
+
+        return not rev'''
+
+# 데크를 이용한 최적화
+'''class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        q = deque()
+
+        if not head:
+            return True
+
+        node = head
+        # 리스트 변환
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+
+        # 팰린드롬 판별
+        while len(q) > 1:
+            if q.popleft() != q.pop():
+                return False
+
+        return True'''
+
+# 리스트 변환
+'''class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        q: List = []
+
+        if not head:
+            return True
+
+        node = head
+        # 리스트 변환
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+
+        # 팰린드롬 판별
+        while len(q) > 1:
+            if q.pop(0) != q.pop():
+                return False
+
+        return True'''
+
+# 다시 풀이
