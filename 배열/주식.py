8077dd8e01adf27aebad13d26f58d29ab53c841f
Park-min-hyoung <qkralswgud1324@naver.com>
2021-11-10 08:16:16 +0900

    plus

--- 배열/주식.py
+++ 배열/주식.py
@@ -15,3 +15,7 @@ print(profit)
 # 틀렸음
 '''1. 저장된 저점(price가 가장 낮은 지점)을 현재 price에서 빼는 아이디어
 (기존의 저점보다 현재 price가 작으면 저점은 수정된다)'''
+
+'''https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2
+&contestProbId=AV5LrsUaDxcDFAXc&categoryId=AV5LrsUaDxcDFAXc&categoryType=CO
+DE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=2&pageSize=10&pageIndex=1'''
