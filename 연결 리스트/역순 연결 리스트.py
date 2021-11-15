f2cfd6c2c7c501b8985624c2f3f410c4e02acd61
Park-min-hyoung <qkralswgud1324@naver.com>
2021-11-15 18:20:29 +0900

    plus

--- 연결 리스트/역순 연결 리스트.py
+++ 연결 리스트/역순 연결 리스트.py
@@ -10,7 +10,19 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         rev = None
 
-        while head != None:
+        while head:
             rev, rev.next, head = head, rev, head.next
 
         return rev
+
+# 재귀 구조로 뒤집기
+'''class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        def reverse(node: ListNode, prev: ListNode = None):
+            if not node:
+                return prev
+
+            next, node.next = node.next, prev
+            return reverse(next, node)
+
+        return reverse(head)'''
