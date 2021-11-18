5fd52d47a6a057e383eb2538aef255bdb3d8533d
Park-min-hyoung <qkralswgud1324@naver.com>
2021-11-18 15:04:03 +0900

    plus

---
+++ 스택, 큐/스택을 이용한 큐 구현.py
@@ -0,0 +1,19 @@
+# 232
+
+class MyQueue:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        for i in range(len())
+
+    def pop(self) -> int:
+        return self.stack.pop(0)
+
+    def peek(self) -> int:
+        return self.stack[0]
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0
