cce0efa2a502753aae46f0ef48976eb0018b793b
Park-min-hyoung <qkralswgud1324@naver.com>
2021-11-20 13:38:42 +0900

    plus

---
+++ 스택, 큐/일일 온도.py
@@ -0,0 +1,18 @@
+# 739
+
+temperatures = [73,74,75,71,69,72,76,73]
+stack = []
+result = [0] * len(temperatures)
+
+for i, temp in enumerate(temperatures):
+    while stack and temp > temperatures[stack[-1]]:
+        last = stack.pop()
+        result[last] = i - last
+    stack.append(i)
+
+print(result)
+
+# 틀렸음
+'''
+1. 현재 온도를 기준으로 바로 전의 온도(stack[-1])가 낮을 경우 stack에서 이전 온도를 가져와서(stack.pop()) 현재 온도와의 차이를 result에 넣어준다.
+그리고 현재 온도는 stack에 채워준다.'''
---
+++ 스택, 큐/중복 문자 제거.py
@@ -0,0 +1,43 @@
+#316
+from collections import Counter
+
+# 스택을 이용한 문자 제거
+s = "cbacdcbc"
+counter, stack, seen = Counter(s), [], set()
+
+for char in s:
+    counter[char] -= 1
+
+    if char in seen:
+        continue
+
+    while stack and char < stack[-1] and counter[stack[-1]] > 0:
+        seen.remove(stack.pop())
+    stack.append(char)
+    seen.add(char)
+
+print(''.join(stack))
+
+# 재귀를 이용한 분리
+'''def removeDuplicateLetters(s):
+    for char in sorted(set(s)):
+        suffix = s[s.index(char):]
+
+        if set(s) == set(suffix):
+            return char + removeDuplicateLetters(suffix.replace(char, ''))
+    return ''
+
+print(removeDuplicateLetters(strs))'''
+
+# 틀렸음
+'''
+재귀를 이용한 분리
+1. set를 이용해 중복문자를 제거 및 정렬 후 접미사 배열을 만들어 원래 배열과 함께 set을 사용해 같은지를 체크 한다.
+2. 다르다면 접미사 쪽에서 어떠한 문자를 처리할 수 없는 경우로 만족하지 못하므로 해당 함수의 다음 반복문을 수행하고
+같다고 하면 해당 문자를 return 하는 동시에 해당 문자가 제거된(replace()) suffix를 다음 함수의 문자열로 넘겨준다.
+3. 1~2번의 과정을 반복하고 더 이상 suffix가 없을 때 백트래킹 되면서 결과가 조합된다
+스택을 이용한 문자 제거
+1. Counter 클래스를 사용해 현재 문자를 기준으로 뒤쪽에 스택에 최근에 입력된 문자가 있는지 check 할 수 있으며, set을 통해
+현재 문자가 set 있으면 다음 반복문으로 갈 수 있게끔 해 중복을 피할 수 있다.
+3. 현재 문자가 최근 스택에 쌓인 문자보다 사전순으로 빠르고 스택에 쌓인 문자가 아직 반복되지 않은 문자열 원소 중에(현재 문자 뒤쪽)
+있다면 스택에서 그 문자를 빼낸다.'''
