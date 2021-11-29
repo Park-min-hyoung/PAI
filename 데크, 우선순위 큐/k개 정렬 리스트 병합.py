af5f2337645debe942122e54aa04dd9fcd3831c0
Park-min-hyoung <qkralswgud1324@naver.com>
2021-11-29 19:25:16 +0900

    plus

--- 데크, 우선순위 큐/k개 정렬 리스트 병합.py
+++ 데크, 우선순위 큐/k개 정렬 리스트 병합.py
@@ -16,3 +16,26 @@ class Solution:
             head = head.next
 
         return root.next
+
+# 우선순위 큐를 이용한 리스트 병합
+'''import heapq
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        root = result = ListNode(None)
+        heap = []
+
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+
+        while heap:
+            node = heapq.heappop(heap)
+            idx = node[1]
+            result.next = node[2]
+
+            result = result.next
+            if result.next:
+                heapq.heappush(heap, (result.next.val, idx, result.next))
+
+        return root.next'''
