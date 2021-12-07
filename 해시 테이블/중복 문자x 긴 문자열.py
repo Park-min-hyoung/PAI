4a6d02c1e733ff26b647a3d678febdedb4634f1f
Park-min-hyoung <qkralswgud1324@naver.com>
2021-12-07 21:05:39 +0900

    plus

--- 해시 테이블/중복 문자x 긴 문자열.py
+++ 해시 테이블/중복 문자x 긴 문자열.py
@@ -1,4 +1,5 @@
 #3
+
 import collections
 
 s = "abcabcbb"
@@ -16,7 +17,7 @@ for idx, char in enumerate(s, 1):
 
 print(max_len)
 
-# 틀렸음
+# 틀렸음(x)
 '''1. 해시를 이용해 현재 index의 문자를 방문했었는지 check 할 수 있다.
 2. 현재 index의 문자가 해시에 있다면 진행중인 슬라이싱에 문자가 포함되어 있으므로 해쉬에 저장되어있는 index 값 + 1로 first를 초기화한다
 (단 해시 저장되어 있는 index는 슬라이딩에 포함되어 있어야 하므로 first보다 크거나 같아야한다.(슬라이딩에 포함되어 있지 않으면 안된다))
