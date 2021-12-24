50cc834c87f5bc2ad8d8e838e480a2e58d9589b9
Park-min-hyoung <qkralswgud1324@naver.com>
2021-12-25 06:52:09 +0900

    plus

---
+++ 트리/BST를 더 큰 수 합계 트리.py
@@ -0,0 +1,13 @@
+#1038
+
+class Solution:
+    val = 0
+
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        if root:
+            self.bstToGst(root.right)
+            self.val += root.val
+            root.val = self.val
+            self.bstToGst(root.left)
+
+        return root
