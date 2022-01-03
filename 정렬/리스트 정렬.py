6a13eef70756a0a91e0bf2469e9d5a6c9c890365
Park-min-hyoung <qkralswgud1324@naver.com>
2022-01-03 21:19:18 +0900

    plus

---
+++ 정렬/리스트 정렬.py
@@ -0,0 +1,48 @@
+#148
+
+# 병합 정렬
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+
+        return l1 or l2
+
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not (head and head.next):
+            return head
+
+        half, slow, fast = None, head, head
+        while fast and fast.next:
+            half, slow, fast = slow, slow.next, fast.next.next
+        half.next = None
+
+        l1 = self.sortList(head)
+        l2 = self.sortList(slow)
+
+        return self.mergeTwoLists(l1, l2)
+
+# 내장 함수를 이용
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p = head
+        lst = []
+
+        while p:
+            lst.append(p.val)
+            p = p.next
+
+        lst.sort()
+
+        p = head
+        for i in range(len(lst)):
+            p.val = lst[i]
+            p = p.next
+        return head
