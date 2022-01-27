e57780cd11141ed7dee245e4d6322f8a386223c6
Park-min-hyoung <qkralswgud1324@naver.com>
2022-01-27 09:59:35 +0900

    plus

---
+++ 분할정복/과반수 엘리먼트.py
@@ -0,0 +1,12 @@
+# #169
+#
+# from collections import Counter
+#
+# nums = [1,2,3]
+# count = Counter(nums).most_common(1)
+# print(count[0][0]) if count[0][1] > len(nums) // 2 else print(11111)
+
+a = "-2-3-4"
+
+a = a.replace("-", "")
+print(a.isdigit())
